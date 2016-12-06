0d3d0bd04c2ffef7f5085fc821da83639c35a443
Gourav Suri <gourav96suri@yahoo.com>
2016-12-06 21:00:23 +0530

    Revert "Changes to app structure"

--- app/src/main/java/com/google/android/gms/samples/vision/ocrreader/OcrGraphic.java
+++ app/src/main/java/com/google/android/gms/samples/vision/ocrreader/OcrGraphic.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.android.gms.samples.vision.ocrreader;
 
 import android.graphics.Canvas;
@@ -11,6 +26,10 @@ import com.google.android.gms.vision.text.TextBlock;
 
 import java.util.List;
 
+/**
+ * Graphic instance for rendering TextBlock position, size, and ID within an associated graphic
+ * overlay view.
+ */
 public class OcrGraphic extends GraphicOverlay.Graphic {
 
     private int mId;
@@ -54,6 +73,13 @@ public class OcrGraphic extends GraphicOverlay.Graphic {
         return mText;
     }
 
+    /**
+     * Checks whether a point is within the bounding box of this graphic.
+     * The provided point should be relative to this graphic's containing overlay.
+     * @param x An x parameter in the relative context of the canvas.
+     * @param y A y parameter in the relative context of the canvas.
+     * @return True if the provided point is contained within this graphic's bounding box.
+     */
     public boolean contains(float x, float y) {
         TextBlock text = mText;
         if (text == null) {
@@ -67,6 +93,9 @@ public class OcrGraphic extends GraphicOverlay.Graphic {
         return (rect.left < x && rect.right > x && rect.top < y && rect.bottom > y);
     }
 
+    /**
+     * Draws the text block annotations for position, size, and raw value on the supplied canvas.
+     */
     @Override
     public void draw(Canvas canvas) {
         TextBlock text = mText;
